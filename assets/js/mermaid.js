af74b78614f5fda001a5f62891f7e0c7108c51c6
thedeathsheep <helloandone@gmail.com>
2025-04-10 14:39:47 +0800

    Test

---
+++ assets/js/mermaid.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    mermaid.initialize({
+        startOnLoad: true,
+        theme: 'default',
+        securityLevel: 'loose',
+        flowchart: {
+            useMaxWidth: true,
+            htmlLabels: true,
+            curve: 'basis'
+        }
+    });
+}); 
